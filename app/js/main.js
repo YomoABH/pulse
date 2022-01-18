024f92fd6c47bee4f7887d821bccfd2a92380c2a
Nikita Sidiropulo <sidiropulonikita04@gmail.com>
2022-01-18 13:32:26 +0300

    отремонтировал забаговавшие модальные окна, адаптировал их в промежутке 375-576px. так же адаптирорвал и стилизовал слайдер для мобилок

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,17 @@
 $(document).ready(function () {
 	$('.slider__inner').slick({
 		speed: 900,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					arrows: false,
+					infinite: true,
+					dots: true
+				}
+			},
+		]
+
 	});
 
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
@@ -31,7 +42,6 @@ $(document).ready(function () {
 		$('.overline,  #consultation').fadeIn(400);
 	});
 
-
 	$('.modal__close').on('click', function () {
 		$('.overline, #consultation, #order, #thinks').fadeOut(300);
 	});
@@ -41,14 +51,5 @@ $(document).ready(function () {
 			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
 			$('.overline,  #order').fadeIn(400);
 		})
-
 	});
-
-	// $('.btn_mini').each(function (i) {
-	// 	$(this).on('click', function () {
-	// 		$('#order, .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-	// 		$('.overline,  #order').fadeIn(400);
-	// 	})
-	// });
-
 });
